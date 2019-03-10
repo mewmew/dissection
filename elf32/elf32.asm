eeeee7becd0babac4efd70a58e241e3ca55dee73
Robin Eklind <rnd0x00@gmail.com>
2019-03-10 15:02:25 +0900

    elf32: cleanup

--- elf32/elf32.asm
+++ elf32/elf32.asm
@@ -18,37 +18,37 @@ r_seg:
 ; === [ ELF file header ] ======================================================
 
 ; ELF classes.
-ELFCLASS32 equ 1 ; 32-bit object
+ELFCLASS32 equ 1 ; 32-bit architecture.
 
 ; Data encodings.
-ELFDATA2LSB equ 1 ; 2's complement with little-endian encoding
+ELFDATA2LSB equ 1 ; 2's complement little-endian.
 
 ; Object file types.
-ET_EXEC equ 2 ; Executable file
+ET_EXEC equ 2 ; Executable.
 
-; Architecture.
-EM_386 equ 3 ; Intel i386
+; CPU architectures.
+EM_386 equ 3 ; Intel i386.
 
 ehdr:
 
-	db      0x7F, "ELF"               ; ident.magic: ELF magic number
-	db      ELFCLASS32                ; ident.class: File class
-	db      ELFDATA2LSB               ; ident.data: Data encoding
-	db      1                         ; ident.version: ELF header version
-	db      0, 0, 0, 0, 0, 0, 0, 0, 0 ; ident.pad: Padding
-	dw      ET_EXEC                   ; type: Object file type
-	dw      EM_386                    ; machine: Architecture
-	dd      1                         ; version: Object file version
-	dd      text.start                ; entry: Entry point virtual address
-	dd      phdr_off                  ; phoff: Program header table file offset
-	dd      0                         ; shoff: Section header table file offset
-	dd      0                         ; flags: Processor-specific flags
-	dw      ehdr.size                 ; ehsize: ELF header size in bytes
-	dw      phdr.entsize              ; phentsize: Program header table entry size
-	dw      phdr.count                ; phnum: Program header table entry count
-	dw      0                         ; shentsize: Section header table entry size
-	dw      0                         ; shnum: Section header table entry count
-	dw      0                         ; shstrndx: Section header string table index
+	db      0x7F, "ELF"               ; ident.magic: ELF magic number.
+	db      ELFCLASS32                ; ident.class: File class.
+	db      ELFDATA2LSB               ; ident.data: Data encoding.
+	db      1                         ; ident.version: ELF header version.
+	db      0, 0, 0, 0, 0, 0, 0, 0, 0 ; ident.pad: Padding.
+	dw      ET_EXEC                   ; type: File type.
+	dw      EM_386                    ; machine: Machine architecture.
+	dd      1                         ; version: ELF format version.
+	dd      _text.start               ; entry: Entry point.
+	dd      phdr_off                  ; phoff: Program header file offset.
+	dd      0                         ; shoff: Section header file offset.
+	dd      0                         ; flags: Architecture-specific flags.
+	dw      ehdr.size                 ; ehsize: Size of ELF header in bytes.
+	dw      phdr.entsize              ; phentsize: Size of program header entry.
+	dw      phdr.count                ; phnum: Number of program header entries.
+	dw      0                         ; shentsize: Size of section header entry.
+	dw      0                         ; shnum: Number of section header entries.
+	dw      0                         ; shstrndx: Section name strings section.
 
 .size equ $ - ehdr
 
@@ -57,14 +57,14 @@ ehdr:
 ; === [ Program headers ] ======================================================
 
 ; Segment types.
-PT_LOAD    equ 1 ; Loadable program segment
-PT_DYNAMIC equ 2 ; Dynamic linking information
-PT_INTERP  equ 3 ; Program interpreter
+PT_LOAD    equ 1 ; Loadable segment.
+PT_DYNAMIC equ 2 ; Dynamic linking information segment.
+PT_INTERP  equ 3 ; Pathname of interpreter.
 
 ; Segment flags.
-PF_R equ 0x4 ; Segment is readable
-PF_W equ 0x2 ; Segment is writable
-PF_X equ 0x1 ; Segment is executable
+PF_R equ 0x4 ; Readable.
+PF_W equ 0x2 ; Writable.
+PF_X equ 0x1 ; Executable.
 
 phdr_off equ phdr - BASE_R_SEG
 
@@ -73,64 +73,64 @@ phdr:
 ; --- [ Interpreter program header ] -------------------------------------------
 
   .interp:
-	dd      PT_INTERP                ; type: Segment type
-	dd      interp_off               ; offset: Segment file offset
-	dd      interp                   ; vaddr: Segment virtual address
-	dd      interp                   ; paddr: Segment physical address
-	dd      interp.size              ; filesz: Segment size in file
-	dd      interp.size              ; memsz: Segment size in memory
-	dd      PF_R                     ; flags: Segment flags
-	dd      0x1                      ; align: Segment alignment
+	dd      PT_INTERP   ; type: Segment type
+	dd      interp_off  ; offset: Segment file offset
+	dd      interp      ; vaddr: Segment virtual address
+	dd      interp      ; paddr: Segment physical address
+	dd      interp.size ; filesz: Segment size in file
+	dd      interp.size ; memsz: Segment size in memory
+	dd      PF_R        ; flags: Segment flags
+	dd      0x1         ; align: Segment alignment
 
 .entsize equ $ - phdr
 
 ; --- [ Dynamic array program header ] -----------------------------------------
 
   .dynamic:
-	dd      PT_DYNAMIC             ; type: Segment type
-	dd      dynamic_off            ; offset: Segment file offset
-	dd      dynamic                ; vaddr: Segment virtual address
-	dd      dynamic                ; paddr: Segment physical address
-	dd      dynamic.size           ; filesz: Segment size in file
-	dd      dynamic.size           ; memsz: Segment size in memory
-	dd      PF_R                   ; flags: Segment flags
-	dd      0x4                    ; align: Segment alignment
+	dd      PT_DYNAMIC   ; type: Segment type
+	dd      dynamic_off  ; offset: Segment file offset
+	dd      dynamic      ; vaddr: Segment virtual address
+	dd      dynamic      ; paddr: Segment physical address
+	dd      dynamic.size ; filesz: Segment size in file
+	dd      dynamic.size ; memsz: Segment size in memory
+	dd      PF_R         ; flags: Segment flags
+	dd      0x4          ; align: Segment alignment
 
 ; --- [ Read-only segment program header ] -------------------------------------
 
   .r_seg:
-	dd      PT_LOAD                  ; type: Segment type
-	dd      r_seg_off                ; offset: Segment file offset
-	dd      r_seg                    ; vaddr: Segment virtual address
-	dd      r_seg                    ; paddr: Segment physical address
-	dd      r_seg.size               ; filesz: Segment size in file
-	dd      r_seg.size               ; memsz: Segment size in memory
-	dd      PF_R                     ; flags: Segment flags
-	dd      PAGE                     ; align: Segment alignment
+	dd      PT_LOAD    ; type: Segment type
+	dd      r_seg_off  ; offset: Segment file offset
+	dd      r_seg      ; vaddr: Segment virtual address
+	dd      r_seg      ; paddr: Segment physical address
+	dd      r_seg.size ; filesz: Segment size in file
+	dd      r_seg.size ; memsz: Segment size in memory
+	dd      PF_R       ; flags: Segment flags
+	dd      PAGE       ; align: Segment alignment
 
 ; --- [ Read-write segment program header ] ------------------------------------
 
   .rw_seg:
-	dd      PT_LOAD                  ; type: Segment type
-	dd      rw_seg_off               ; offset: Segment file offset
-	dd      rw_seg                   ; vaddr: Segment virtual address
-	dd      rw_seg                   ; paddr: Segment physical address
-	dd      rw_seg.size              ; filesz: Segment size in file
-	dd      rw_seg.size              ; memsz: Segment size in memory
-	dd      PF_R | PF_W              ; flags: Segment flags
-	dd      PAGE                     ; align: Segment alignment
+	dd      PT_LOAD     ; type: Segment type
+	dd      rw_seg_off  ; offset: Segment file offset
+	dd      rw_seg      ; vaddr: Segment virtual address
+	dd      rw_seg      ; paddr: Segment physical address
+	dd      rw_seg.size ; filesz: Segment size in file
+	dd      rw_seg.size ; memsz: Segment size in memory
+	dd      PF_R | PF_W ; flags: Segment flags
+	dd      PAGE        ; align: Segment alignment
 
 ; --- [ Executable segment program header ] ------------------------------------
 
   .rx_seg:
-	dd      PT_LOAD                  ; type: Segment type
-	dd      rx_seg_off               ; offset: Segment file offset
-	dd      rx_seg                   ; vaddr: Segment virtual address
-	dd      rx_seg                   ; paddr: Segment physical address
-	dd      rx_seg.size              ; filesz: Segment size in file
-	dd      rx_seg.size              ; memsz: Segment size in memory
-	dd      PF_R | PF_X              ; flags: Segment flags
-	dd      PAGE                     ; align: Segment alignment
+	dd      PT_LOAD     ; type: Segment type
+	dd      rx_seg_off  ; offset: Segment file offset
+	dd      rx_seg      ; vaddr: Segment virtual address
+	dd      rx_seg      ; paddr: Segment physical address
+	dd      rx_seg.size ; filesz: Segment size in file
+	dd      rx_seg.size ; memsz: Segment size in memory
+	dd      PF_R | PF_X ; flags: Segment flags
+	dd      PAGE        ; align: Segment alignment
 
 .size  equ $ - phdr
 .count equ .size / .entsize
@@ -154,42 +154,42 @@ interp:
 ; --- [ .dynamic section ] -----------------------------------------------------
 
 ; Dynamic tags.
-DT_NULL     equ 0  ; Marks the end of the dynamic array
-DT_NEEDED   equ 1  ; String table offset of a required library
-DT_PLTGOT   equ 3  ; Address of the PLT and/or GOT
-DT_STRTAB   equ 5  ; Address of the string table
-DT_SYMTAB   equ 6  ; Address of the symbol table
-DT_JMPREL   equ 23 ; Address of the relocation entities of the PLT
+DT_NULL   equ 0  ; Terminating entry.
+DT_NEEDED equ 1  ; String table offset of a needed shared library.
+DT_PLTGOT equ 3  ; Processor-dependent address.
+DT_STRTAB equ 5  ; Address of string table.
+DT_SYMTAB equ 6  ; Address of symbol table.
+DT_JMPREL equ 23 ; Address of PLT relocations.
 
 dynamic_off equ dynamic - BASE_R_SEG
 
 dynamic:
 
   .strtab:
-	dd      DT_STRTAB              ; tag: Dynamic entry type
-	dd      dynstr                 ; val: Integer or address value
+	dd      DT_STRTAB ; tag: Entry type.
+	dd      dynstr    ; val: Integer/Address value.
 
 .entsize equ $ - dynamic
 
   .symtab:
-	dd      DT_SYMTAB              ; tag: Dynamic entry type
-	dd      dynsym                 ; val: Integer or address value
+	dd      DT_SYMTAB ; tag: Entry type.
+	dd      dynsym    ; val: Integer/Address value.
 
   .jmprel:
-	dd      DT_JMPREL              ; tag: Dynamic entry type
-	dd      rel_plt                ; val: Integer or address value
+	dd      DT_JMPREL ; tag: Entry type.
+	dd      rel_plt   ; val: Integer/Address value.
 
   .pltgot:
-	dd      DT_PLTGOT              ; tag: Dynamic entry type
-	dd      got_plt                ; val: Integer or address value
+	dd      DT_PLTGOT ; tag: Entry type.
+	dd      got_plt   ; val: Integer/Address value.
 
   .libc:
-	dd      DT_NEEDED              ; tag: Dynamic entry type
-	dd      dynstr.libc_off        ; val: Integer or address value
+	dd      DT_NEEDED       ; tag: Entry type.
+	dd      dynstr.libc_off ; val: Integer/Address value.
 
   .null:
-	dd      DT_NULL                ; tag: Dynamic entry type
-	dd      0                      ; val: Integer or address value
+	dd      DT_NULL ; tag: Entry type.
+	dd      0       ; val: Integer/Address value.
 
 .size equ $ - dynamic
 
@@ -199,6 +199,7 @@ dynamic:
 
 dynstr:
 
+; libc.so.6
   .libc:
 	db      "libc.so.6", 0
   .printf:
@@ -206,6 +207,7 @@ dynstr:
   .exit:
 	db      "exit", 0
 
+; libc.so.6
 .libc_off   equ .libc - dynstr
 .printf_off equ .printf - dynstr
 .exit_off   equ .exit - dynstr
@@ -215,34 +217,34 @@ dynstr:
 ; --- [ .dynsym section ] ------------------------------------------------------
 
 ; Symbol bindings.
-STB_GLOBAL equ 1 ; Global symbol
+STB_GLOBAL equ 1 ; Global symbol.
 
 ; Symbol types.
-STT_FUNC equ 2 ; Code object
+STT_FUNC equ 2 ; Function.
 
 ; Symbol visibility.
 STV_DEFAULT equ 0 ; Default visibility.
 
 dynsym:
 
+; libc.so.6
   .printf:
-	dd      dynstr.printf_off        ; name: Symbol name (string table offset)
-	dd      0                        ; value: Symbol value
-	dd      0                        ; size: Symbol size
-	db      STB_GLOBAL<<4 | STT_FUNC ; info: Symbol type and binding
-	db      STV_DEFAULT              ; other: Symbol visibility
-	dw      0                        ; shndx: Section index
-
+	dd      dynstr.printf_off        ; name: String table offset of name.
+	dd      0                        ; value: Symbol value.
+	dd      0                        ; size: Size of associated object.
+	db      STT_FUNC | STB_GLOBAL<<4 ; info: Type and binding information.
+	db      STV_DEFAULT              ; other: Symbol visibility.
+	dw      0                        ; shndx: Section index of symbol.
 .entsize equ $ - dynsym
-
   .exit:
-	dd      dynstr.exit_off          ; name: Symbol name (string table offset)
-	dd      0                        ; value: Symbol value
-	dd      0                        ; size: Symbol size
-	db      STB_GLOBAL<<4 | STT_FUNC ; info: Symbol type and binding
-	db      STV_DEFAULT              ; other: Symbol visibility
-	dw      0                        ; shndx: Section index
+	dd      dynstr.exit_off          ; name: String table offset of name.
+	dd      0                        ; value: Symbol value.
+	dd      0                        ; size: Size of associated object.
+	db      STT_FUNC | STB_GLOBAL<<4 ; info: Type and binding information.
+	db      STV_DEFAULT              ; other: Symbol visibility.
+	dw      0                        ; shndx: Section index of symbol.
 
+; libc.so.6
 .printf_idx equ (.printf - dynsym) / .entsize
 .exit_idx   equ (.exit - dynsym) / .entsize
 
@@ -251,31 +253,32 @@ dynsym:
 ; --- [ .rel.plt section ] -----------------------------------------------------
 
 ; Relocation types.
-R_386_JMP_SLOT equ 7
+R_386_JMP_SLOT equ 7 ; Set GOT entry to code address.
 
 rel_plt:
 
+; libc.so.6
   .printf:
-	dd      got_plt.printf                         ; offset: Address
-	dd      dynsym.printf_idx<<8 | R_386_JMP_SLOT  ; info: Relocation type and symbol index
-
+	dd      got_plt.printf                        ; offset: Location to be relocated.
+	dd      R_386_JMP_SLOT | dynsym.printf_idx<<8 ; info: Relocation type and symbol index.
   .exit:
-	dd      got_plt.exit                           ; offset: Address
-	dd      dynsym.exit_idx<<8 | R_386_JMP_SLOT    ; info: Relocation type and symbol index
+	dd      got_plt.exit                        ; offset: Location to be relocated.
+	dd      R_386_JMP_SLOT | dynsym.exit_idx<<8 ; info: Relocation type and symbol index.
 
+; libc.so.6
 .printf_off equ .printf - rel_plt
 .exit_off   equ .exit - rel_plt
 
 ; --- [/ .rel.plt section ] ----------------------------------------------------
 
-; --- [ .rodata section ] ------------------------------------------------------
+; --- [ .rdata section ] -------------------------------------------------------
 
-rodata:
+_rdata:
 
   .hello:
 	db      "hello world", 10, 0
 
-; --- [/ .rodata section ] -----------------------------------------------------
+; --- [/ .rdata section ] ------------------------------------------------------
 
 r_seg.size equ $ - r_seg
 
@@ -302,9 +305,9 @@ got_plt:
   .dl_runtime_resolve:
 	dd      0
 
+; libc.so.6
   .printf:
 	dd      plt.resolve_printf
-
   .exit:
 	dd      plt.resolve_exit
 
@@ -330,16 +333,14 @@ plt:
 	push    dword [got_plt.link_map]
 	jmp     [got_plt.dl_runtime_resolve]
 
+; libc.so.6
   .printf:
 	jmp     [got_plt.printf]
-
   .resolve_printf:
 	push    dword rel_plt.printf_off
 	jmp     near .resolve
-
   .exit:
 	jmp     [got_plt.exit]
-
   .resolve_exit:
 	push    dword rel_plt.exit_off
 	jmp     near .resolve
@@ -348,13 +349,13 @@ plt:
 
 ; --- [ .text section ] --------------------------------------------------------
 
-text:
+_text:
 
   .start:
-	push    rodata.hello   ; arg1, "hello world\n"
+	push    _rdata.hello   ;    arg1, "hello world\n"
 	call    plt.printf     ; printf
 	add     esp, 4
-	push    42             ; arg1, 42
+	push    42             ;    arg1, 42
 	call    plt.exit       ; exit
 	add     esp, 4
 	ret
